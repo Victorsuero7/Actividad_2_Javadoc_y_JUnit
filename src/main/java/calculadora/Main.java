e430524fe26a9ddb63eb230352d60c34e3dda1d2
Marcos Lambir Torres <132981758+MLT93@users.noreply.github.com>
2025-02-22 14:14:58 +0100

    refactor: update requested changes

--- src/main/java/calculadora/Main.java
+++ src/main/java/calculadora/Main.java
@@ -2,13 +2,6 @@ package calculadora;
 
 public class Main {
     public static void main(String[] args) {
-
-        /* TEST */
-        double doubleMultiplyTwoParams = Multiply.doubleTwoParamsProduct(3.3, 1.5);
-        int intMultiplyTwoParams = Multiply.intTwoParamsProduct(3, 3);
-        double doubleMultiplyThreeParams = Multiply.doubleThreeParamsProduct(2.3, 7.2, 3.14);
-        System.out.println(doubleMultiplyTwoParams);
-        System.out.println(intMultiplyTwoParams);
-        System.out.println(doubleMultiplyThreeParams);
+        
     }
 }
